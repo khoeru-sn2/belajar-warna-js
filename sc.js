bfc876ad3496808d2276262c8e9f8f4c640e0ee5
khoeru-sn2 <86897399+khoeru-sn2@users.noreply.github.com>
2021-09-10 20:04:20 +0700

    mengubah warna background dengan javascript

---
+++ sc.js
@@ -0,0 +1,55 @@
+// membuat tombol ketika diklik sekali backgroundnya berubah menjadi biru muda dan ketika diklik lagi backgroundnya berubah menjadi warna putih kembali
+
+const tombolU = document.getElementById("tombolU");
+tombolU.onclick = function () {
+  document.body.classList.toggle("biru-muda");
+};
+
+// membuat tombol ketika diklik akan merubah warna backgroundnya menjadi warna random
+
+const tAcakWarna = document.createElement("button");
+const teksTombol = document.createTextNode("Ubah Warna Random");
+tAcakWarna.appendChild(teksTombol);
+tAcakWarna.setAttribute("type", "button");
+tombolU.after(tAcakWarna);
+
+tAcakWarna.addEventListener("click", function () {
+  const r = Math.round(Math.random() * 255 + 1);
+  const g = Math.round(Math.random() * 255 + 1);
+  const b = Math.round(Math.random() * 255 + 1);
+  document.body.style.backgroundColor = "rgb(" + r + "," + g + "," + b + ")";
+});
+
+// merubah background dengan slider
+const sMerah = document.querySelector("input[name=sMerah]");
+const sHijau = document.querySelector("input[name=sHijau]");
+const sBiru = document.querySelector("input[name=sBiru]");
+
+sMerah.addEventListener("input", function () {
+  const r = sMerah.value;
+  const g = sHijau.value;
+  const b = sBiru.value;
+  document.body.style.backgroundColor = "rgb(" + r + ", " + g + "," + b + ")";
+});
+
+sHijau.addEventListener("input", function () {
+  const r = sMerah.value;
+  const g = sHijau.value;
+  const b = sBiru.value;
+  document.body.style.backgroundColor = "rgb(" + r + "," + g + "," + b + ")";
+});
+
+sBiru.addEventListener("input", function () {
+  const r = sMerah.value;
+  const g = sHijau.value;
+  const b = sBiru.value;
+  document.body.style.backgroundColor = "rgb(" + r + "," + g + "," + b + ")";
+});
+
+// ketika mouse digerakan backgroundnya akan berubah warna
+document.body.addEventListener("mousemove", function (event) {
+  const xPos = Math.round((event.clientX / window.innerWidth) * 255);
+  const yPos = Math.round((event.clientY / window.innerHeight) * 255);
+
+  document.body.style.backgroundColor = "rgb(" + xPos + "," + yPos + ", 100)";
+});
